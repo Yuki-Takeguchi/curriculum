30cb37f2a712769005d9b956c21bf1059a5ee8e4
Yuki-Takeguchi <myu.151.0828@gmail.com>
2021-06-13 22:16:22 +0900

    動作確認

---
+++ JavaStudy /HellJava.java/src/module-info.java
@@ -0,0 +1,16 @@
+// クラス
+public class HelloJava {
+
+    // フィールド
+    private String message;
+
+    // コンストラクタ
+    public HelloJava() {
+        message = "Hello, Java!";
+    }
+
+    // メソッド
+    public void print() {
+        System.out.println(message);
+    }
+}
