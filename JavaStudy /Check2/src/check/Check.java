88506f6bf5b0a2606c529e1bfa3134a29ee82dc7
Yuki-Takeguchi <myu.151.0828@gmail.com>
2021-06-29 21:41:20 +0900

    2章チェックテスト　コメント追加

--- JavaStudy /Check2/src/check/Check.java
+++ JavaStudy /Check2/src/check/Check.java
@@ -1,27 +1,40 @@
 package check;
 
 import constants.Constants;
-
+//constantsパッケージのConstants.javaの定数を利用するためインポートする
 public class Check {
 	
+	//課題１【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
+	//firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
+	//なお、変数のアクセス修飾子は「private」とする。
+	//main関数で値を利用するのでstaticを付ける
 	private static String firstName = "武口";
     private static String lastName = "雄喜";
 	
+    
     public static void main(String args[]) {
+    	//課題２のメソッドの呼び出し
 		printName(firstName,lastName);
 		
+		//課題３【Check.java】にてPetクラスとRobotPetクラスをインスタンス化して、下記の完成イメージを出力させなさい。
+		
+		//petクラスをインスタンス化
 		Pet Pet1 = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		
-		Pet1.getName();
-		
-		Pet1.getMasterName();
-				
+		//Petクラスのメソッドを使って出力
 		Pet1.introduce();
 		
+		//RobotPetクラスをインスタンス化
 		RobotPet RobotPet1 = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		
+		//RoboPetクラスのメソッドを使って出力
 		RobotPet1.introduce();
 	}
 	
+    //課題２【Check.java】にてfirstNameとlastNameを引数で受け取って、
+    //連結して表示させるメソッド「printName」を作成しなさい。
+    //作成した関数のアクセス修飾子は「private」とする。
+    //privateなのでクラス内部からのみ、アクセスが可能です。
 	private static void printName(String FirstName, String LastName) {
 		System.out.println("printNameメソッド → " + FirstName + LastName);
 	} 
--- JavaStudy /Check2/src/check/Pet.java
+++ JavaStudy /Check2/src/check/Pet.java
@@ -5,6 +5,7 @@ public class Pet {
     private String name;
     private String masterName;
 
+    //コンストラクタ
     public Pet(String name, String masterName) {
         this.name = name;
         this.masterName = masterName;
@@ -26,6 +27,7 @@ public class Pet {
 
 class RobotPet extends Pet {
     public RobotPet(String name, String masterName) {
+    	//親クラスを継承することで、変数にアクセスすることが可能になる。
         super(name, masterName);
     }
 
